aae2e43840cb030876a61280a48a97f8367ba5b1
Bui Minh Quan <32923349+mq149@users.noreply.github.com>
2022-06-13 16:48:51 +0700

    Initial commit for Web API.

---
+++ Tqhdl01DiChoOnline/Tqhdl01DiChoOnline/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Tqhdl01DiChoOnline.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public OrderController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("test")]
+        public JsonResult Get()
+        {
+            string query = @"SELECT * FROM orders LIMIT 100";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TQHDL_V5");
+            MySqlDataReader myReader;
+            using (MySqlConnection myCon = new MySqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using(MySqlCommand myCmd = new MySqlCommand(query, myCon))
+                {
+                    myReader = myCmd.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ Tqhdl01DiChoOnline/Tqhdl01DiChoOnline/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Tqhdl01DiChoOnline.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
